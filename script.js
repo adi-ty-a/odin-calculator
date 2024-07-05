30f6bad986266c82dea768ff77a7c0bb9fd4ffe5
adi-ty-a <adi.personal.13@gmail.com>
2024-07-05 22:10:18 +0530

    click -> func update p || equal cal operate

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-let n1,n2,op;
+let display,n1,n2,op;
 
 
 function operate(no1,no2,operator){
@@ -8,22 +8,55 @@ function operate(no1,no2,operator){
     if(operator === 'divide') return divide(no1,no2);
 }
 
-    let trigger = document.queryselector('button')
-    trigger.addEventListener('click',function(){
-        
-    })
+let press = document.querySelectorAll('button')
+press.forEach((press)=>
+press.addEventListener('click',function(){
+    if(press.textContent == '+'){
+        op='sum';
+        let text = document.querySelector('.main-display')
+        n1 = text.textContent 
+        text.textContent = ''
+    }
+    if(press.textContent == '-'){
+        op='subtract';
+        let text = document.querySelector('.main-display')
+        n1 = text.textContent 
+        text.textContent = ''
+    }
+    if(press.textContent == '*'){
+        op='multiply';
+        let text = document.querySelector('.main-display')
+        n1 = text.textContent 
+        text.textContent = ''
+    }
+    if(press.textContent == '/'){
+        op='divide';
+        let text = document.querySelector('.main-display')
+        n1 = text.textContent 
+        text.textContent = ''
+    }
+    if(press.textContent == '='){
+        let text = document.querySelector('.main-display')
+        n2 = text.textContent 
+        text.textContent = operate(n1,n2,op)
+
+    }
+    else{let text = document.querySelector('.main-display')
+    text.textContent = this.textContent;}
+}))
+
 
 
 function sum(n1,n2){
-    return n1+n2;
+    return parseInt(n1)+parseInt(n2);
 }
 function multiply(n1,n2){
-    return n1*n2;
+    return parseInt(n1)*parseInt(n2);
 }
 function subtract(n1,n2){
-    return n1-n2;
+    return parseInt(n1)-parseInt(n2);
 }
 function divide(n1,n2){
-    return n1/n2;
+    return parseInt(n1)/parseInt(n2);
 }
 
